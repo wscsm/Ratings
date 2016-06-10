532d5d021e67c29e59c19fdf726198d0ab0cf873
kbytes <325189@qq.com>
2016-06-10 12:14:58 +0800

    modify ui

--- Ratings/Meal.swift
+++ Ratings/Meal.swift
@@ -15,9 +15,13 @@ class Meal {
     var rating: Int
     
     // MARK: Initialization
-    init(name: String, photo: UIImage?, rating: Int){
+    init?(name: String, photo: UIImage?, rating: Int){
         self.name = name
         self.photo = photo
         self.rating = rating
+        
+        if (name.isEmpty || rating < 0) {
+            return nil
+        }
     }
 }
