9065732fd070a1a9a1b4ec87e065c9139e7724f9
kbytes <325189@qq.com>
2016-06-10 14:01:17 +0800

    meal tableviewcontioler added.

---
+++ Ratings/MealTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  MealTableViewCell.swift
+//  Ratings
+//
+//  Created by suyanlu on 16/6/10.
+//  Copyright © 2016年 suyanlu. All rights reserved.
+//
+
+import UIKit
+
+class MealTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
